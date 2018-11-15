2a34aea7cc78539436874f7a165d4b25834a5b5c
James Magallanes <fridaysyckness@gmail.com>
2018-11-15 08:50:58 +0800

    Updates

---
+++ precache-manifest.7ad35ef7af3721079e679ad3bd6a9297.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "942972880d64bcd3b4e0",
+    "url": "/sample-react/static/css/main.a1a6e971.chunk.css"
+  },
+  {
+    "revision": "942972880d64bcd3b4e0",
+    "url": "/sample-react/static/js/main.94297288.chunk.js"
+  },
+  {
+    "revision": "3dec32785c6957e49d8f",
+    "url": "/sample-react/static/css/1.9f1040c6.chunk.css"
+  },
+  {
+    "revision": "3dec32785c6957e49d8f",
+    "url": "/sample-react/static/js/1.3dec3278.chunk.js"
+  },
+  {
+    "revision": "ab3bb469658debc715cb",
+    "url": "/sample-react/static/js/runtime~main.ab3bb469.js"
+  },
+  {
+    "revision": "61c97262f84d694f3b0edd4c06837549",
+    "url": "/sample-react/index.html"
+  }
+];
